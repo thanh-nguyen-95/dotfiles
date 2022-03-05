ca01bff241b0222463dde9c6c53f653fe9e81b54
Thanh Nguyen <thanh.nguyen@egany.com>
2022-03-05 11:41:16 +0700

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,31 @@
+# Clean
+rm -rf .config/bspwm
+rm -rf .config/sxhkd
+rm -rf .config/polybar
+rm -rf .config/alacritty
+rm -rf .config/nvim
+rm -rf .config/rofi
+rm -rf .config/picom.conf
+rm -rf ~/.gitconfig
+rm -rf ~/.ngrok2
+rm -rf ~/ngrok
+rm -rf ~/.zshrc
+rm -rf ~/.zsh_aliases
+rm -rf ~/.prettierrc
+rm -rf ~/redshift.conf
+
+# Link
+ln -s ~/dotfiles/.config/bspwm ~/.config/bspwm
+ln -s ~/dotfiles/.config/sxhkd ~/.config/sxhkd
+ln -s ~/dotfiles/.config/polybar ~/.config/polybar
+ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/dotfiles/.config/rofi ~/.config/rofi
+ln -s ~/dotfiles/.config/picom.conf ~/.config/picom.conf
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.ngrok2 ~/.ngrok2
+ln -s ~/dotfiles/ngrok ~/ngrok
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zsh_aliases ~/.zsh_aliases
+ln -s ~/dotfiles/.prettierrc ~/.prettierrc
+ln -s ~/dotfiles/redshift.conf ~/redshift.conf
