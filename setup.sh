9499bbab8ee329a19db987ee51af6266a4b40da5
Thanh Nguyen <thanh.nguyen@egany.com>
2022-06-10 11:08:05 +0700

    Add fontconfig, wallpapers

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ rm -rf ~/.config/rofi
 rm -rf ~/.config/ranger
 rm -rf ~/.config/picom.conf
 rm -rf ~/.config/starship.toml
+rm -rf ~/.config/fontconfig/conf.d/30-family-defaults.conf
 rm -rf ~/.gitconfig
 rm -rf ~/.ngrok2
 rm -rf ~/ngrok
@@ -16,6 +17,7 @@ rm -rf ~/.zshrc
 rm -rf ~/.zsh_aliases
 rm -rf ~/.prettierrc
 rm -rf ~/redshift.conf
+rm -rf ~/wallapers
 
 # Link
 ln -s ~/dotfiles/.config/awesome ~/.config/awesome
@@ -28,6 +30,7 @@ ln -s ~/dotfiles/.config/rofi ~/.config/rofi
 ln -s ~/dotfiles/.config/ranger ~/.config/ranger
 ln -s ~/dotfiles/.config/picom.conf ~/.config/picom.conf
 ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+ln -s ~/dotfiles/.config/fontconfig/conf.d/30-family-defaults.conf
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.ngrok2 ~/.ngrok2
 ln -s ~/dotfiles/ngrok ~/ngrok
@@ -35,6 +38,7 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zsh_aliases ~/.zsh_aliases
 ln -s ~/dotfiles/.prettierrc ~/.prettierrc
 ln -s ~/dotfiles/redshift.conf ~/redshift.conf
+ln -s ~/dotfiles/wallpapers ~/wallpapers
 
 # Fonts
 mkdir -p ~/.local/share/fonts
