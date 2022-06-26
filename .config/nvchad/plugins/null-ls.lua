da18900b26938bfc5317148d144f68cc49cf2352
Thanh Nguyen <thanh.nguyen@egany.com>
2022-06-26 09:15:37 +0700

    Update config

--- .config/nvchad/plugins/null-ls.lua
+++ .config/nvchad/plugins/null-ls.lua
@@ -14,12 +14,12 @@ M.setup = function()
 	null_ls.setup({
 		debug = true,
 		sources = sources,
-		-- format on save
-		on_attach = function(client)
-			if client.resolved_capabilities.document_formatting then
-				vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-			end
-		end,
+		-- -- format on save
+		-- on_attach = function(client)
+		-- 	if client.resolved_capabilities.document_formatting then
+		-- 		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		-- 	end
+		-- end,
 	})
 end
 
