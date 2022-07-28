773b931cda5ec5c4ce92ffd4df8571701732c1cb
Thanh Nguyen <thanh.nguyen@egany.com>
2022-07-28 17:05:01 +0700

    Update config

--- .config/nvchad/plugins/init.lua
+++ .config/nvchad/plugins/init.lua
@@ -5,7 +5,9 @@ return {
 			require("null-ls").setup({
 				sources = {
 					require("null-ls").builtins.formatting.stylua,
-					require("null-ls").builtins.formatting.prettier,
+					require("null-ls").builtins.formatting.prettier.with({
+            prefer_local = true
+          }),
 				},
 			})
 		end,
