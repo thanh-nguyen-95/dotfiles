2851fb0a5ba5b171a94f193db7034384cbe768c4
Thanh Nguyen <thanh.nguyen@egany.com>
2022-10-09 18:04:00 +0700

    Update config

--- work.sh
+++ work.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
+
+# List all directories
 projects=`ls -d ~/projects/*`
 researches=`ls -d ~/reseaches/*`
 
+# Select a directory using FZF
 selected=`printf "$projects\n$researches" | fzf`
 
-echo $selected
-
+# Default 'cd' to selected directory
 cd $selected
-nvim $selected
+
+# Ask for extra command. Ex: nvim
+read -p "Extra command: " cmd
+
+
+# Execute extra command with selected directory
+if [ -z $cmd ]
+  then
+    exit 1
+else
+  $cmd $selected
+fi
