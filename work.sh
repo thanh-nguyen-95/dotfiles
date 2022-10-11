a97e4a3f4f06186769ae0285c063694fefa4b9c5
Thanh Nguyen <thanh.nguyen@egany.com>
2022-10-11 21:32:00 +0700

    Update config

--- work.sh
+++ work.sh
@@ -2,7 +2,7 @@
 
 # List all directories
 projects=`ls -d ~/projects/*`
-researches=`ls -d ~/reseaches/*`
+researches=`ls -d ~/researches/*`
 
 # Select a directory using FZF
 selected=`printf "$projects\n$researches" | fzf`
@@ -13,29 +13,5 @@ then
   exit 1
 fi
 
-# Ask for extra command. Ex: nvim
-options=`echo "goto;open in neovim;start dev server" | tr ';' '\n'`
-commands=`printf "$options" | fzf`
-
-# Execute extra command with selected directory
-case "$commands" in
-  "goto")
-    cd "$selected"
-    # Required for cd command to work
-    $SHELL    
-    ;;
-
-  "open in neovim")
-    cd "$selected"
-    nvim "$selected"
-    ;;
-
-  "start dev server")
-    cd "$selected"
-    npm run dev
-    ;;
-
-  *)
-    echo "default"
-    ;;
-esac
+cd "$selected"
+$SHELL
