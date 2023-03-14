b69f3f3cc1f6022f3b77703b6e38a6f66845e8b6
Thanh Nguyen <thanh.nguyen@egany.com>
2023-03-14 12:51:08 +0700

    Update config

--- setup-endeavour.arch.sh
+++ setup-endeavour.arch.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Has yay, start installing packages"
 
-yay -S bspwm sxhkd polybar rofi alacritty xclip flameshot zsh neofetch mpv telegram-desktop ibus-bamboo docker docker-compose python-pywal fzf ripgrep postman-bin starship ranger pamixer brightnessctl noto-fonts noto-fonts-emoji noto-fonts-cjk feh dunst code neovim google-chrome cloudflare-warp-bin
+yay -S bspwm sxhkd polybar rofi alacritty zsh dunst xclip flameshot neofetch mpv telegram-desktop ibus-bamboo docker docker-compose python-pywal fzf ripgrep postman-bin starship ranger pamixer brightnessctl noto-fonts noto-fonts-emoji noto-fonts-cjk feh code neovim google-chrome cloudflare-warp-bin htop
 
 echo "Installing Fast Node Manager"
 
@@ -24,13 +24,15 @@ sudo systemctl enable --now docker.service
 sudo systemctl enable --now docker.socket
 sudo systemctl enable --now warp-svc.service
 
+# Cloudflare VPN
+sudo systemctl enable --now warp-svc.service
+
 # CONFIG
 # ================================
 
 echo "Configuring packages"
 
 # Clean
-rm -rf ~/.config/awesome
 rm -rf ~/.config/bspwm
 rm -rf ~/.config/sxhkd
 rm -rf ~/.config/polybar
@@ -38,21 +40,20 @@ rm -rf ~/.config/alacritty
 rm -rf ~/.config/nvim
 rm -rf ~/.config/rofi
 rm -rf ~/.config/flameshot
-rm -rf ~/.config/picom.conf
 rm -rf ~/.config/redshift.conf
 rm -rf ~/.config/mimeapps.list
 rm -rf ~/.gitconfig
-rm -rf ~/.ngrok2
-rm -rf ~/ngrok
+# rm -rf ~/.bashrc
+# rm -rf ~/.bash_aliases
 rm -rf ~/.zshrc
 rm -rf ~/.zsh_aliases
+rm -rf ~/.ngrok2
+rm -rf ~/ngrok
 rm -rf ~/.prettierrc
-rm -rf ~/.xprofile
 rm -rf ~/work.sh
 rm -rf ~/config.sh
 
 # Link
-ln -s ~/dotfiles/.config/awesome ~/.config/awesome
 ln -s ~/dotfiles/.config/bspwm ~/.config/bspwm
 ln -s ~/dotfiles/.config/sxhkd ~/.config/sxhkd
 ln -s ~/dotfiles/.config/polybar ~/.config/polybar
@@ -60,16 +61,16 @@ ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/dotfiles/.config/rofi ~/.config/rofi
 ln -s ~/dotfiles/.config/flameshot ~/.config/flameshot
-ln -s ~/dotfiles/.config/picom.conf ~/.config/picom.conf
 ln -s ~/dotfiles/.config/redshift.conf ~/.config/redshift.conf
 ln -s ~/dotfiles/.config/mimeapps.list ~/.config/mimeapps.list
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.ngrok2 ~/.ngrok2
-ln -s ~/dotfiles/ngrok ~/ngrok
+# ln -s ~/dotfiles/.bashrc ~/.bashrc
+# ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zsh_aliases ~/.zsh_aliases
+ln -s ~/dotfiles/.ngrok2 ~/.ngrok2
+ln -s ~/dotfiles/ngrok ~/ngrok
 ln -s ~/dotfiles/.prettierrc ~/.prettierrc
-ln -s ~/dotfiles/.xprofile ~/.xprofile
 ln -s ~/dotfiles/work.sh ~/work.sh
 ln -s ~/dotfiles/config.sh ~/config.sh
 
@@ -82,7 +83,9 @@ sudo chmod +x ~/.config/polybar/launch.sh
 sudo chmod +x ~/work.sh
 sudo chmod +x ~/config.sh
 
-# Shells
+# Shell
+# sudo chsh -s /bin/bash
+# sudo chsh -s /bin/bash $USER
 sudo chsh -s /bin/zsh
 sudo chsh -s /bin/zsh $USER
 
