127643e9d212db2ca5fbfd7e57bf00f3043aeaf0
Thành Nguyễn <thanh.nguyen@egany.com>
2024-01-11 15:09:40 +0700

    Update alacritty font-size and neovim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -82,8 +82,7 @@ require("lazy").setup({
       "williamboman/mason-lspconfig.nvim",
 
       -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { "j-hui/fidget.nvim", opts = {}, tag = "legacy", event = "LspAttach" },
+      { "j-hui/fidget.nvim", opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       "folke/neodev.nvim",
@@ -100,7 +99,7 @@ require("lazy").setup({
     -- Surround support
     "kylechui/nvim-surround",
     config = function()
-      require("nvim-surround").setup({})
+      require("nvim-surround").setup()
     end,
   },
 
@@ -175,7 +174,7 @@ require("lazy").setup({
       require("neo-tree").setup({
         enable_git_status = false,
         window = {
-          width = 60,
+          width = 40,
           mappings = {
             ["l"] = "open",
             ["h"] = "close_node",
@@ -273,9 +272,6 @@ require("lazy").setup({
   {
     -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
     config = function()
       pcall(require("nvim-treesitter.install").update({ with_sync = true }))
     end,
@@ -410,63 +406,19 @@ vim.keymap.set("n", "<leader>pd", require("telescope.builtin").diagnostics, { de
 require("nvim-treesitter.configs").setup({
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { "tsx", "typescript", "vim", "prisma", "css", "html", "json", "dockerfile" },
+  sync_install = false,
+  modules = {},
+  ignore_install = {},
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
   highlight = { enable = true },
-  indent = { enable = true, disable = { "python" } },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<c-space>",
-      node_incremental = "<c-space>",
-      scope_incremental = "<c-s>",
-      node_decremental = "<M-space>",
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner",
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
-      },
-    },
-    -- swap = {
-    --   enable = true,
-    --   swap_next = {
-    --     ["<leader>a"] = "@parameter.inner",
-    --   },
-    --   swap_previous = {
-    --     ["<leader>A"] = "@parameter.inner",
-    --   },
-    -- },
-  },
+  disable = function(_, buf)
+    local max_filesize = 100 * 1024 -- 100 KB
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    if ok and stats and stats.size > max_filesize then
+      return true
+    end
+  end,
 })
 
 -- Diagnostic keymaps
