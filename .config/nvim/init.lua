e282ce4b214dbf1bbb7bb606c790ba74f105aa53
Thành Nguyễn <thanh.nguyen@egany.com>
2024-03-11 11:56:39 +0700

    Update codedium config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -143,13 +143,14 @@ require("lazy").setup({
   {
     'Exafunction/codeium.vim',
     config = function()
-      -- Disable press tab to accept completion
-      vim.keymap.set('i', '<C-.>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+      vim.g.codeium_no_map_tab = 1
+
+      vim.keymap.set('i', '<C-\\>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
         { expr = true, silent = true })
-      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+      vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
         { expr = true, silent = true })
-      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
     end
   },
 
