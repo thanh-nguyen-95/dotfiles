e76321901bb5ea457b5d49bc7e3b46af4965ecc2
Thành Nguyễn <thanh.nguyen@egany.com>
2024-10-18 19:57:35 +0700

    Add vim-fugitive. Update plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -232,7 +232,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	"tpope/vim-fugitive",
 
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
