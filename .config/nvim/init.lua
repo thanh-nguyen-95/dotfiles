d260ec43fe2401e5df625eed43a3f1b12ace5882
Thành Nguyễn <thanh.nguyen@egany.com>
2024-11-18 19:11:47 +0700

    Update neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -665,7 +665,7 @@ require("lazy").setup({
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
-				"prettierd", -- Used to format various front-end code
+				"prettier", -- Used to format various front-end code
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
