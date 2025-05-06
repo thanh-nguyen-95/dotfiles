690b5efd67c3bb475ae95deebbc0dbf52dd52c9f
Thành Nguyễn <thanh.nguyen@egany.com>
2025-05-06 11:31:25 +0700

    Upgrade neovim packages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -93,6 +93,9 @@ vim.g.maplocalleader = " "
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Enable true colors
+vim.g.termguicolors = true
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
