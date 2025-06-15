5cea5d026fb72f85f774477de8a24d8cd5436b32
Thành Nguyễn <thanh.nguyen@egany.com>
2025-06-15 22:20:13 +0700

    Update script

--- setup-endeavour.kde.sh
+++ setup-endeavour.kde.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Has yay, start installing packages"
 
-yay -S zsh neofetch mpv docker docker-compose fzf ripgrep starship neovim google-chrome htop fd zellij fcitx5 fcitx5-unikey fcitx5-configtool ghostty ttf-font-awesome brave-bin
+yay -S zsh neofetch mpv docker docker-compose fzf ripgrep starship neovim google-chrome htop fd zellij ibus-bamboo ghostty ttf-font-awesome brave-bin
 
 echo "Installing Fast Node Manager"
 
@@ -34,7 +34,6 @@ echo "Configuring packages"
 # Clean
 rm -rf ~/.config/ghostty
 rm -rf ~/.config/nvim
-rm -rf ~/.config/fcitx5
 rm -rf ~/.config/zellij
 rm -rf ~/.config/neofetch
 rm -rf ~/.gitconfig
@@ -46,7 +45,6 @@ rm -rf ~/config.sh
 # Link
 ln -s ~/dotfiles/.config/ghostty ~/.config/ghostty
 ln -s ~/dotfiles/.config/nvim ~/.config/nvim
-ln -s ~/dotfiles/.config/fcitx5 ~/.config/fcitx5
 ln -s ~/dotfiles/.config/zellij ~/.config/zellij
 ln -s ~/dotfiles/.config/neofetch ~/.config/neofetch
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
