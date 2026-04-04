6bc39d97273acfa324a9ec97b3e927c472807ef6
Thành Nguyễn <thanh.nguyen@egany.com>
2026-04-04 09:05:46 +0700

    Add neovim. Update ghostty config

---
+++ .config/nvim/lua/plugins/tn.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    opts = {
+      log_level = "off", -- set to "off" to disable logging completely
+      disable_inline_completion = false, -- disables inline completion for use with cmp
+      keymaps = {
+        accept_suggestion = "<C-\\>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-'>",
+      },
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+      },
+    },
+  },
+}
